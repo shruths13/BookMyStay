ded4c8cf15007d43c067c52a123c244fa2b815c1
shruths13 <shruths7@gmail.com>
2026-03-24 15:02:11 +0530

    UC2: Basic Room Types and Static Availability

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,72 @@
-public class Main{
+abstract class Room {
+    protected String roomType;
+    protected int numberOfBeds;
+    protected double price;
 
-        public static void main(String[] args) {
-
-            // Welcome Message
-            System.out.println("=======================================");
-            System.out.println("   Welcome to Book My Stay App");
-            System.out.println("=======================================");
-
-            // Application Info
-            System.out.println("Application Version: 1.0");
-
-            // Startup Confirmation
-            System.out.println("System started successfully.");
-        }
+    public Room(String roomType, int numberOfBeds, double price) {
+        this.roomType = roomType;
+        this.numberOfBeds = numberOfBeds;
+        this.price = price;
     }
 
+    public void displayDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Price: $" + price);
+    }
+}
+
+// Single Room
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super("Single Room", 1, 100.0);
+    }
+}
+
+// Double Room
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super("Double Room", 2, 180.0);
+    }
+}
+
+// Suite Room
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super("Suite Room", 3, 300.0);
+    }
+}
+
+// Main Application
+public class Main {
+    public static void main(String[] args) {
+
+        // Create room objects (Polymorphism)
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        // Availability (simple variables ONLY)
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        System.out.println("===== Room Availability System =====\n");
+
+        // Display Single Room
+        single.displayDetails();
+        System.out.println("Available Rooms: " + singleAvailable);
+        System.out.println();
+
+        // Display Double Room
+        doubleRoom.displayDetails();
+        System.out.println("Available Rooms: " + doubleAvailable);
+        System.out.println();
+
+        // Display Suite Room
+        suite.displayDetails();
+        System.out.println("Available Rooms: " + suiteAvailable);
+
+        System.out.println("\n===== End of Program =====");
+    }
+}
