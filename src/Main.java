056b469752bb28fe1fd08523c01affac5ebb7f85
shruths13 <shruths7@gmail.com>
2026-03-24 14:12:20 +0530

    UC1: welcome message

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main{
+
+        public static void main(String[] args) {
+
+            // Welcome Message
+            System.out.println("=======================================");
+            System.out.println("   Welcome to Book My Stay App");
+            System.out.println("=======================================");
+
+            // Application Info
+            System.out.println("Application Version: 1.0");
+
+            // Startup Confirmation
+            System.out.println("System started successfully.");
+        }
+    }
+
